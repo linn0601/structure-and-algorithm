fe58af12c4251281b8a7fd403473f02015699f50
linn <linn0601@qq.com>
2021-05-11 16:20:24 +0800

    feat:完善递归和相关题解

--- src/structure/recursion/Recursion.java
+++ src/structure/recursion/Recursion.java
@@ -5,21 +5,37 @@ package structure.recursion;
  */
 public class Recursion {
 
-	/**
-	 *  爬楼梯，假设你正在爬楼梯，需要n阶你才能达到楼顶部，你每次可以爬1/2 台阶，你有多少种不同的方法可以爬到楼顶 n = 正整数
-	 */
-	public static int climbStairs(int n, int i) {
+	public static void main(String[] args) {
+		System.out.println(climbStairs2(2));
+	}
 
-		// 结束条件
-		if (i > n) {
-			return 0;
-		}
-		if (n == i) {
+	/**
+	 * 爬楼梯，假设你正在爬楼梯，需要n阶你才能达到楼顶部，你每次可以爬1/2 台阶，你有多少种不同的方法可以爬到楼顶 n = 正整数
+	 *
+	 * 递归 ：斐波那契 动态规划法 ：
+	 */
+	public static int climbStairs(int n) {
+		if (n == 0 | n == 1) {
 			return 1;
 		}
 
-		// 递归调用
-		return climbStairs(i + 1, i) + climbStairs(i + 2, i);
+		return climbStairs(n - 1) + climbStairs(n - 2);
+	}
+
+	/**
+	 * 动态规划法 dp[n] = dp[n-1] + dp[n-2]
+	 */
+	public static int climbStairs2(int n) {
+
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		return dp[n];
 	}
 
 	/**
